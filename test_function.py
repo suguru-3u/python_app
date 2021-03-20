7517b95673c0438497bf5305ea7e66de1f57b03c
Oohira Suguru <suguru.13579515@gmail.com>
2021-03-20 11:23:49 +0900

    Create app end function test

--- test_function.py
+++ test_function.py
@@ -31,6 +31,16 @@ class TestCalc(unittest.TestCase):
     print('test_cheak_input_blenkのテスト開始')
     self.assertEqual(False, function.cheak_input_blenk(TestCalc.task))   
     print('test_cheak_input_blenkのテスト終了')
+
+  def test_task_app_end_judgment_1(self):
+    print('task_app_end_judgmentのテスト開始')
+    self.assertEqual(1, function.task_app_end_judgment())   
+    print('task_app_end_judgmentのテスト終了')
+
+  def test_task_app_end_judgment_2(self):
+    print('task_app_end_judgmentのテスト開始')
+    self.assertEqual(2, function.task_app_end_judgment())   
+    print('task_app_end_judgmentのテスト終了')
   
 if __name__ == '__main__':
     unittest.main()
