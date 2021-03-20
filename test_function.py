2a2348f566dd07365d402af0b44856f6fef4dcb0
Oohira Suguru <suguru.13579515@gmail.com>
2021-03-20 20:19:46 +0900

    Create test

--- test_function.py
+++ test_function.py
@@ -33,20 +33,20 @@ class TestCalc(unittest.TestCase):
     self.assertEqual(True, function.cheak_input_blenk(TestCalc.blank_task))   
     print('test_cheak_input_blenkのテスト終了')
 
-  # def test_cheak_input(self):
-  #   print('test_cheak_input_blenkのテスト開始')
-  #   self.assertEqual(False, function.cheak_input_blenk(TestCalc.task))   
-  #   print('test_cheak_input_blenkのテスト終了')
+  def test_cheak_input(self):
+    print('test_cheak_input_blenkのテスト開始')
+    self.assertEqual(False, function.cheak_input_blenk(TestCalc.task))   
+    print('test_cheak_input_blenkのテスト終了')
 
-  # def test_task_app_end_judgment_1(self):
-  #   print('task_app_end_judgmentのテスト開始')
-  #   self.assertEqual(1, function.task_app_end_judgment())   
-  #   print('task_app_end_judgmentのテスト終了')
+  def test_task_app_end_judgment_1(self):
+    print('task_app_end_judgmentのテスト開始')
+    self.assertEqual(1, function.task_app_end_judgment())   
+    print('task_app_end_judgmentのテスト終了')
 
-  # def test_task_app_end_judgment_2(self):
-  #   print('task_app_end_judgmentのテスト開始')
-  #   self.assertEqual(2, function.task_app_end_judgment())   
-  #   print('task_app_end_judgmentのテスト終了')
+  def test_task_app_end_judgment_2(self):
+    print('task_app_end_judgmentのテスト開始')
+    self.assertEqual(2, function.task_app_end_judgment())   
+    print('task_app_end_judgmentのテスト終了')
 
   def test_get_tasks(self):
     print('test_get_tasksのテスト開始')
