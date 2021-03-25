4a41c52ae9b569062cb6204e724b7d51cbfa30fb
suguru-3u <suguru.13579515@gmail.com>
2021-03-25 17:57:04 +0900

    add test

--- main.py
+++ main.py
@@ -47,3 +47,33 @@ while True:
         task_app_function.blank()
         print('**********終了する場合はqを入力してください**********')
         
+while True:
+    try:
+        sleep(3)
+        task_app_function.blank()
+        print(task_app_function.border)
+        task_db.get_tasks()
+        print(task_app_function.border)
+        task_app_function.blank()
+        select = input(introduction)
+        if select == '1' :
+            task_document.Create_task()
+        elif select == '2':
+            task_document.Edit_task()
+        elif select == '3':
+            task_document.Delete_task()
+        elif select == 'q':
+            end_judgment = task_app_function.task_app_end_judgment()
+            if end_judgment == 1:
+                task_db.sql_stop()
+                break
+            else:
+                continue
+        else:
+            task_app_function.blank()
+            print('正しく入力してください')
+    except KeyboardInterrupt:
+        task_app_function.blank()
+        task_app_function.blank()
+        print('**********終了する場合はqを入力してください**********')
+        
